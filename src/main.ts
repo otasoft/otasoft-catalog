5652e27839e8ff3de380f1b864773d6f0ecd8449
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-25 17:01:42 +0100

    feat: add microservice connection

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
+import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { catalogMicroserviceOptions } from './microservice-connection/microservice-connection';
+
+const logger = new Logger('CatalogMicroservice')
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.createMicroservice(
+    AppModule,
+    catalogMicroserviceOptions,
+  );
+
+  await app.listen(() => {
+    logger.log('Microservice is listening');
+  });
 }
 bootstrap();
