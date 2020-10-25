07e63e0f4ab93049a540333274bc1a3c04359a8b
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-10-25 19:01:23 +0100

    feat: configure database module to work with mysql image

---
+++ src/db/db.module.ts
@@ -0,0 +1,23 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('MYSQL_HOST'),
+        port: configService.get('MYSQL_PORT'),
+        username: configService.get('MYSQL_USER'),
+        password: configService.get('MYSQL_PASSWORD'),
+        database: configService.get('MYSQL_DATABASE'),
+        synchronize: true,
+        autoLoadEntities: true,
+      }),
+    }),
+  ],
+})
+export class DbModule {}
