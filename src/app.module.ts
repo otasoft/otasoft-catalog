d310b780b78cff8db45e33cabad7a155e597fd92
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-01 12:57:24 +0100

    feat-#2: configure app.module to use CqrsModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { CqrsModule } from '@nestjs/cqrs';
 import { DbModule } from './db/db.module';
 import { CatalogModule } from './catalog/catalog.module';
 import { HealthModule } from './health/health.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(), DbModule, CatalogModule, HealthModule],
+  imports: [
+    ConfigModule.forRoot(),
+    CqrsModule,
+    DbModule,
+    CatalogModule,
+    HealthModule,
+  ],
 })
 export class AppModule {}
