7e8bc09eb996b3913627a2ac4f52052858d469f1
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-01 13:47:26 +0100

    feat-#2: add missing commands imports

--- src/catalog/activity/commands/handlers/index.ts
+++ src/catalog/activity/commands/handlers/index.ts
@@ -1,1 +1,9 @@
-export const CommandHandlers = [];
+import { CreateActivityHandler } from "./create-activity.handler";
+import { DeleteActivityHandler } from "./delete-activity.handler";
+import { UpdateActivityHandler } from "./update-activity.handler";
+
+export const CommandHandlers = [
+    CreateActivityHandler,
+    DeleteActivityHandler,
+    UpdateActivityHandler
+];
--- src/catalog/cars/commands/handlers/index.ts
+++ src/catalog/cars/commands/handlers/index.ts
@@ -1,1 +1,9 @@
-export const CommandHandlers = [];
+import { CreateCarsHandler } from "./create-cars.handler";
+import { DeleteCarsHandler } from "./delete-cars.handler";
+import { UpdateCarsHandler } from "./update-cars.handler";
+
+export const CommandHandlers = [
+    CreateCarsHandler,
+    DeleteCarsHandler,
+    UpdateCarsHandler
+];
