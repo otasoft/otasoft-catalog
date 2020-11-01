5048a55772d66f75bd3f74be5b099e62b547abf2
Baroshem <jakub.andrzejewski.dev@gmail.com>
2020-11-01 13:04:19 +0100

    feat-#2: create activity entity and configue activity repository

--- src/catalog/activity/repositories/activity.entity.ts
+++ src/catalog/activity/repositories/activity.entity.ts
@@ -1,7 +1,20 @@
-
-import { BaseEntity, Entity } from "typeorm";
+import {
+  BaseEntity,
+  Column,
+  Entity,
+  PrimaryGeneratedColumn,
+  Unique,
+} from 'typeorm';
 
 @Entity()
+@Unique(['name'])
 export class ActivityEntity extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  activity_id: number;
 
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
 }
--- src/catalog/activity/repositories/activity.repository.ts
+++ src/catalog/activity/repositories/activity.repository.ts
@@ -1,5 +1,5 @@
-import { EntityRepository, Repository } from "typeorm";
-import { ActivityEntity } from "./activity.entity";
+import { EntityRepository, Repository } from 'typeorm';
+import { ActivityEntity } from './activity.entity';
 
 @EntityRepository(ActivityEntity)
 export class ActivityRepository extends Repository<ActivityEntity> {}
