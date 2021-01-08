90b171da9bff1b2b45859fad5e5621768d966ffd
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-08 16:54:54 +0100

    feat-#30: move db config to separate file

--- src/db/db.module.ts
+++ src/db/db.module.ts
@@ -1,39 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { CatalogEntities } from './entities';
+
 import { CatalogSubscribers } from './subscribers';
+import { dbAsyncConfig } from './config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: (configService: ConfigService) => ({
-        type: 'mysql',
-        entities: [...CatalogEntities],
-        synchronize: true,
-        replication: {
-          master: {
-            host: configService.get('MASTER_HOST'),
-            port: configService.get('MASTER_PORT'),
-            username: configService.get('MASTER_USER'),
-            password: configService.get('MASTER_PASSWORD'),
-            database: configService.get('MASTER_DATABASE'),
-          },
-          slaves: [
-            {
-              host: configService.get('SLAVE_HOST'),
-              port: configService.get('SLAVE_PORT'),
-              username: configService.get('SLAVE_USER'),
-              password: configService.get('SLAVE_PASSWORD'),
-              database: configService.get('SLAVE_DATABASE'),
-            },
-          ],
-        },
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync(dbAsyncConfig)],
   providers: [...CatalogSubscribers],
 })
 export class DbModule {}
