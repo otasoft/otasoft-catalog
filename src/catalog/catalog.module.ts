9c077ab8508c4b0dc464e4820cceb4636d1c045c
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-09 13:12:47 +0100

    refactor-#35: create DDD modules

--- src/catalog/catalog.module.ts
+++ src/catalog/catalog.module.ts
@@ -1,7 +1,11 @@
 import { Module } from '@nestjs/common';
+
 import { ActivityModule } from './activity/activity.module';
+import { ApplicationModule } from './application/application.module';
+import { InfrastructureModule } from './infrastructure/infrastructure.module';
+import { DomainModule } from './domain/domain.module';
 
 @Module({
-  imports: [ActivityModule],
+  imports: [ActivityModule, ApplicationModule, InfrastructureModule, DomainModule],
 })
 export class CatalogModule {}
