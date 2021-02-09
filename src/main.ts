b2f6f474443b094c452d60b7015a230a2841a233
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-02-09 10:41:07 +0100

    refactor-#35: move microservice options to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,26 @@
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+
 import { AppModule } from './app.module';
-import { catalogMicroserviceOptions } from './microservice-connection/microservice-connection';
 
 const logger = new Logger('CatalogMicroservice');
 
 async function bootstrap() {
   const app = await NestFactory.createMicroservice(
     AppModule,
-    catalogMicroserviceOptions,
+    {
+      transport: Transport.RMQ,
+      options: {
+        urls: [
+          `amqp://${process.env.RABBITMQ_DEFAULT_USER}:${process.env.RABBITMQ_DEFAULT_PASS}@${process.env.RABBITMQ_NODENAME}:${process.env.RABBITMQ_FIRST_HOST_PORT}/${process.env.RABBITMQ_DEFAULT_VHOST}`,
+        ],
+        queue: 'catalog_queue',
+        queueOptions: {
+          durable: false,
+        },
+      },
+    }
   );
 
   await app.listen(() => {
--- src/microservice-connection/README.md
+++
@@ -1,3 +0,0 @@
-# MICROSERVICE-CONNECTION
-
-This directory contains RabbitMQ connection options and in the future will contain rules of connectivity.
